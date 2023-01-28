c8e8602c4f6a4d4d13d5652c3b649be626955bc0
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-01-28 00:16:31 -0300

    inicio de proyecto

---
+++ array.js
@@ -0,0 +1,12 @@
+let stokDeProductos = [
+    { id: 1, img:"./img/lenceria1.jpg", producto: "Conjunto", modelo: "Encaje", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 1200 },
+    { id: 2, img:"./img/lenceria2.jpg", producto: "Conjunto", modelo: "Algodon", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 1500 },
+    { id: 3, img:"./img/lenceria3.jpg", producto: "Conjunto", modelo: "Combinado", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 2000 },
+    { id: 4, img:"./img/lenceria2.jpg", producto: "Top", modelo: "Encaje", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 1400 },
+    { id: 5, img:"./img/lenceria1.jpg", producto: "Top", modelo: "Combinado", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 1600 },
+    { id: 6, img:"./img/lenceria3.jpg", producto: "Bombis", modelo: "Maria", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 900 },
+    { id: 7, img:"./img/lenceria1.jpg", producto: "Bombis", modelo: "Victoria", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 800 },
+    { id: 8, img:"./img/lenceria3.jpg", producto: "Bombis", modelo: "Culote", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 500 },
+    { id: 9, img:"./img/lenceria2.jpg", producto: "Bombis", modelo: "Colaless regulables", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 450 },
+   // { id: 10, img:"", producto: "Bombis", modelo: "Colaless", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 400 }
+]
---
+++ crearProductos.js
@@ -0,0 +1,28 @@
+
+const pintarProductos = () =>{
+    //traigo al contenedor padre
+    const contenedorProducto = document.getElementById("cardProductos")
+    //recorro elarray
+    stokDeProductos.forEach(arrayProducto => {
+        //creo el elem contenedor hijo
+        const contenedorCard =  document.createElement("div")
+        //creo su clase
+        contenedorCard.classList.add("productos__contenedorGral--contCard", "col")
+        contenedorCard.innerHTML = `
+            <div class="productos__contenedorGral--contCard-card card h-100">
+                <img class="img__card card-img-top" src=${arrayProducto.img} alt="producto lenceria">
+                <div class="contenedor__info card-body">
+                    <h5 class="contenedor__info--titulo card-title fs-3">${arrayProducto.producto} 
+                    <span class=" fs-5 text-secondary "> - ${arrayProducto.modelo}</span></h5>
+                    <p class="contenedor__info--precio fs-4">$ ${arrayProducto.precio}</p>
+                    <a href="#" class="contenedor__info--btn btn link-dark" id=${arrayProducto.id}>Agrgar al carrito</a>
+                </div>
+            </div>
+        `
+        //agrego los elementos al padre
+        contenedorProducto.appendChild(contenedorCard);
+
+    })   
+}
+
+pintarProductos()
