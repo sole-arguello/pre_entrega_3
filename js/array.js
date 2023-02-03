88779a3d8250c970f169774b32de19be6dcfcab2
Arguello Maria Soledad Eugenia <soledad.ar1@gmail.com>
2023-02-02 23:39:39 -0300

    limpio el codigo y reacomodo

--- js/array.js
+++ js/array.js
@@ -1,12 +1,12 @@
 let productos = [
-    { id: 1, img:"./img/lenceria1.jpg", cantidad: 1, categoria: { nombre: "Conjunto", idCat:"conjunto" }, titulo: "Conjunto Encaje", precio: 1200 },
-    { id: 2, img:"./img/lenceria2.jpg", cantidad: 1, categoria: { nombre: "Conjunto", idCat:"conjunto" }, titulo: "Conjunto Algodon", precio: 1500 },
-    { id: 3, img:"./img/lenceria3.jpg", cantidad: 1, categoria: { nombre: "Conjunto", idCat:"conjunto" }, titulo: "Conjunto Combinado", precio: 2000 },
-    { id: 4, img:"./img/lenceria2.jpg", cantidad: 1, categoria: { nombre: "Top", idCat:"top" }, titulo: "Top Encaje", precio: 1400 },
-    { id: 5, img:"./img/lenceria1.jpg", cantidad: 1, categoria: { nombre: "Top", idCat:"top" }, titulo: "Top Combinado", precio: 1600 },
-    { id: 6, img:"./img/lenceria3.jpg", cantidad: 1, categoria: { nombre: "Bombis", idCat:"bombis" }, titulo: "Bombis Maria", precio: 900 },
-    { id: 7, img:"./img/lenceria1.jpg", cantidad: 1, categoria: { nombre: "Bombis", idCat:"bombis" }, titulo: "Bombis Victoria", precio: 800 },
-    { id: 8, img:"./img/lenceria3.jpg", cantidad: 1, categoria: { nombre: "Bombis", idCat:"bombis" }, titulo: "Bombis Culote", precio: 500 },
-    { id: 9, img:"./img/lenceria2.jpg", cantidad: 1, categoria: { nombre: "Bombis", idCat:"bombis" }, titulo: "Bombis Colaless regulables", precio: 450 },
+    { id: 1, img:"./img/lenceria1.jpg", cantidad: 1, categoria: "Conjunto", titulo: "Conjunto Encaje", precio: 1200 },
+    { id: 2, img:"./img/lenceria2.jpg", cantidad: 1, categoria: "Conjunto", titulo: "Conjunto Algodon", precio: 1500 },
+    { id: 3, img:"./img/lenceria3.jpg", cantidad: 1, categoria: "Conjunto", titulo: "Conjunto Combinado", precio: 2000 },
+    { id: 4, img:"./img/lenceria2.jpg", cantidad: 1, categoria: "Top", titulo: "Top Encaje", precio: 1400 },
+    { id: 5, img:"./img/lenceria1.jpg", cantidad: 1, categoria: "Top", titulo: "Top Combinado", precio: 1600 },
+    { id: 6, img:"./img/lenceria3.jpg", cantidad: 1, categoria: "Bombis", titulo: "Bombis Maria", precio: 900 },
+    { id: 7, img:"./img/lenceria1.jpg", cantidad: 1, categoria: "Bombis", titulo: "Bombis Victoria", precio: 800 },
+    { id: 8, img:"./img/lenceria3.jpg", cantidad: 1, categoria: "Bombis", titulo: "Bombis Culote", precio: 500 },
+    { id: 9, img:"./img/lenceria2.jpg", cantidad: 1, categoria: "Bombis", titulo: "Bombis Colaless regulables", precio: 450 },
    // { id: 10, img:"", producto: "Bombis", modelo: "Colaless", talle: "S - M - L - Xl", color: "Blanco - Negro - Gris - Beige", precio: 400 }
 ]
--- js/controlador.js
+++ js/controlador.js
@@ -1,17 +1,33 @@
 //traigo al contenedor padre
 const contenedorProductos = document.getElementById("cardProductos")
 const contenedorCarrito = document.getElementById("carritoContenedor")
-const btnVaciarCarrito = document.getElementById("vaciarCarrito")
 const contadorCarrito = document.getElementById("contadorCarrito")
 const totalProductos = document.getElementById("precioTotal")
-const btnConjunto = document.getElementById("btn-conjunto")
-const btnTop = document.getElementById("btn-top")
-const btnBombis = document.getElementById("btn-bombis")
+const btnVaciarCarrito = document.getElementById("vaciarCarrito")
+const btnTodos = document.getElementById("todos")
+const btnConjunto = document.getElementById("conjuntos")
+const btnTop = document.getElementById("top")
+const btnBombis = document.getElementById("bombis")
 let carrito = []
 
 //recorro elarray
-function pintarCrad(){
-    productos.forEach(elProducto => {
+function pintarCrad(categoria){
+    //actualizo al html para spintar segun la categoria que elija
+    contenedorProductos.innerHTML = "";
+    
+    let prodAMostrar;
+    //pregunto si existe la categoria
+    if(categoria){
+        //guardo una copia el array original
+        prodAMostrar = categoria
+        console.log(prodAMostrar)
+    }else{
+        //sino existe la categoria que muestre el array productos
+        prodAMostrar = productos
+        console.log(prodAMostrar)
+    }
+    // recrroro la copia 
+    prodAMostrar.forEach(elProducto => {
         //creo el elem contenedor hijo
         const contenedorCard =  document.createElement("div")
         //creo su clase
@@ -33,28 +49,36 @@ function pintarCrad(){
        `
         //agrego los elementos al padre
         contenedorProductos.appendChild(contenedorCard);
-        //se produce el evento del click en el boton
+        //escucho el evento del click en el boton agregar
         const btnAgregar = document.getElementById(`agregar${elProducto.id}`)
-        btnAgregar.addEventListener("click", () =>{
-            //por cada click en un boton se agrega el producto
-            agregarAlCarrito(elProducto.id)
-            
-        })
+        //por cada click en un boton se agrega el producto
+        btnAgregar.onclick = () =>{ agregarAlCarrito(elProducto.id) }
     }) 
 }
 pintarCrad()
+
+//eventos que escucha los botones de las categorias
+btnTodos.onclick = () => { pintarCrad() }
+btnConjunto.onclick = () =>{ filtrarCategorias("Conjunto")/*console.log("1")*/}
+btnTop.onclick = () =>{ filtrarCategorias("Top") /*console.log("2")*/}
+btnBombis.onclick = () =>{ filtrarCategorias("Bombis") /*console.log("3")*/}
+
+//funcion que filtra la categoria de productos 
+const filtrarCategorias = (categoria) => {
+    const filtrarCateg = productos.filter(prod => prod.categoria === categoria)
+    //console.log(filtrarCateg)
+    pintarCrad(filtrarCateg)
+}
 
 //por parametro le doy el id del prod
 const agregarAlCarrito = (prodId) => {
     //para no repetir el producto
     const existe = carrito.some(prod => prod.id === prodId)
-    
-
     if(existe){
         //encuentro el producto agregado y le sumo la cantidad
         const prod = carrito.map(prod => {
             if(prod.id === prodId){
-                prod.cantidad ++                
+                prod.cantidad ++              
             }
         })
     }else{
@@ -63,18 +87,9 @@ const agregarAlCarrito = (prodId) => {
         carrito.push(item)
         //console.log(carrito)
     }
-    actualizarCarrito()
+    pintarProductosEnCarrito()
 }
-//elimina los del carrito no los del stock
-const eliminarDelCarrito = (prodId) =>{
-    //obtengo el elem del array
-    const itemCarrito = carrito.find((prod) => prod.id === prodId)
-    const indice = carrito.indexOf(itemCarrito)
-    carrito.splice(indice, 1)
-    actualizarCarrito()
-}
-
-const actualizarCarrito = () =>{
+const pintarProductosEnCarrito = () =>{
     //cada vez que llamo a la funcio borro el nodo y lo inicio vacio
     contenedorCarrito.innerHTML = ""
     //recorre el array y lo llena con info actualizada
@@ -82,51 +97,41 @@ const actualizarCarrito = () =>{
         const contItemsCarrito = document.createElement("div")
         contItemsCarrito.classList.add("carritoBody", "row")
         contItemsCarrito.innerHTML = `
+            <img class="body__img col" src="${ arrayCarrito.img }">
             <p class="body__producto col">${arrayCarrito.titulo}</P>
             <p class="body__precio col">Precio: ${arrayCarrito.precio}</p>
             <p class="body__cant col">Cantidad: <span id="cantidad">${arrayCarrito.cantidad}</span></p>
+            <p class="body__subtototal col">Sub-total: <span id="cantidad"> ${arrayCarrito.cantidad * arrayCarrito.precio}</span></p>
             
             <a id="eliminarDelCarrito${arrayCarrito.id}" class="body__btnElim col text-center "><i class="bi bi-trash3"></i></>
         `
         contenedorCarrito.appendChild(contItemsCarrito)
 
-        //elimino uno a uno los elementos del carrito
+        //escucho el boton y elimino uno a uno los elementos del carrito
         const btnElim = document.getElementById(`eliminarDelCarrito${arrayCarrito.id}`)
-        btnElim.onclick = () => {
-            //console.log("elimino")
-            eliminarDelCarrito()
-        }
+        btnElim.onclick = () => {eliminarDelCarrito();/*console.log("elimino")*/}
         
     })
-
-    
+    //muestra el incremento del carrito
     contadorCarrito.innerText = carrito.length
     //por cada producto, el acumulador le sume precio al prod 
-    totalProductos.innerText = carrito.reduce((acc, prod) => acc + prod.precio, 0)
+    totalProductos.innerText = carrito.reduce((acc, prod) => acc + prod.precio * prod.cantidad, 0)
+}
+//elimina los productos del carrito NO del stock
+const eliminarDelCarrito = (prodId) =>{
+    //obtengo el elem del array por medio de su indice
+    const itemCarrito = carrito.find((prod) => prod.id === prodId)
+    const indice = carrito.indexOf(itemCarrito)
+    carrito.splice(indice, 1)
+    pintarProductosEnCarrito()
+}
+//escucho el evento del boron vaciar carrito y borra todos los productos
+btnVaciarCarrito.onclick = () => { carrito.length = 0; pintarProductosEnCarrito();
 }
 
 
-
-//vaciar carrito
-btnVaciarCarrito.addEventListener("click", () => {
-    carrito.length = 0;
-    actualizarCarrito()
-})
+/*------------- Storage ----------------------*/
 
 
 
-//filtrar por categoria
-// const filtrarCateg = productos.filter((elem) => elem.categoria.nombre.includes("Conjuntos"))
-// const filtrarCateg2 = productos.filter((elem) => elem.categoria.nombre.includes("Top"))
-// const filtrarCateg3 = productos.filter((elem) => elem.categoria.idCat.includes("Bombis"))
-// const prod = [
-//     {nombre: "arroz", precio: 500},
-//     {nombre: "harina", precio: 300},
-//     {nombre: "papa", precio: 800},
-//     {nombre: "arroz", precio: 300},
-//     {nombre: "arroz", precio: 1500},
-//     {nombre: "arroz", precio: 1000},
-//     {nombre: "arroz", precio: 2000},
-// ]
 
-//const producto = prod.filter((element) => element.nombre.includes("arroz"))
